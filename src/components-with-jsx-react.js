dcf78463b46334d764c802fbf3386a2a3da224d2
AV-Felipe <aerogrado@hotmail.com>
2022-04-15 08:50:43 -0300

    Change to jsx syntax but loses es6 modules

---
+++ src/components-with-jsx-react.js
@@ -0,0 +1,24 @@
+// import MyListComponent from "./components/default-list-jsx-component.jsx";
+
+// because we need to use type="text/babel" in the index.htm script source, we can't use es6 modules
+// in this project the components must all be inside the main script
+    
+function MyListComponent({items}){
+    return (
+        <ul className="list-component">
+            {items.map((item, index)=>{
+                return (
+                    <li key={index} className="list-item">
+                        {item}
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
+
+
+ReactDOM.render(
+    <MyListComponent items={["item1","item2","item3"]} />,
+    document.getElementById("root")
+);
